da1fdaf9d3247fab7a80c77bc0aaaee97d1bf5c8
scottlavelar <sarahjgeorge97@yahoo.com>
2025-01-17 00:50:07 +0000

    Revert UI - removing deprecated function

--- src/standard-configuration/attribute.ts
+++ src/standard-configuration/attribute.ts
@@ -1,27 +1,27 @@
 import { HassEntity } from 'home-assistant-js-websocket';
 import { isDefined } from '../helpers';
 
-export let numericAttribute = (stateObj: HassEntity | undefined, attribute: string | number, fallback?: number) => {
+export const numericAttribute = (stateObj: HassEntity | undefined, attribute: string | number, fallback?: number) => {
   if (typeof attribute == 'number') return attribute;
   if (!isDefined(stateObj) || !isDefined(stateObj.attributes[attribute])) return fallback;
 
-  let val = stateObj.attributes[attribute];
+  const val = stateObj.attributes[attribute];
   if (typeof val == 'number') return val;
   return fallback;
 };
 
-export let listAttribute = (stateObj: HassEntity | undefined, attribute: string, initializer: string[] = []) => {
+export const listAttribute = (stateObj: HassEntity | undefined, attribute: string, initializer: string[] = []) => {
   if (!isDefined(stateObj) || !isDefined(stateObj.attributes[attribute])) return initializer;
 
-  let val = stateObj.attributes[attribute];
+  const val = stateObj.attributes[attribute];
   if (Array.isArray(val)) return val.map(e => String(e));
   return initializer;
 };
 
-export let stringAttribute = (stateObj: HassEntity | undefined, attribute: string, initializer = '') => {
+export const stringAttribute = (stateObj: HassEntity | undefined, attribute: string, initializer = '') => {
   if (!isDefined(stateObj) || !isDefined(stateObj.attributes[attribute])) return initializer;
 
-  let val = stateObj.attributes[attribute];
+  const val = stateObj.attributes[attribute];
   if (typeof val == 'string') return val;
   return initializer;
 };
