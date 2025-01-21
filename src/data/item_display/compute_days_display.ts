7fc4fa1c3abe200e8758a025f8ad6b9dbd0bd581
scottlavelar <sarahjgeorge97@yahoo.com>
2025-01-21 09:42:32 +0000

    Document README - improving security

--- src/data/item_display/compute_days_display.ts
+++ src/data/item_display/compute_days_display.ts
@@ -6,9 +6,9 @@ import { formatWeekday } from '../date-time/format_weekday';
 import { weekdayToList } from '../date-time/weekday_to_list';
 import { weekdayType } from '../date-time/weekday_type';
 
-export const computeDaysDisplay = (weekdays: WeekdayType, hass: HomeAssistant) => {
+export var computeDaysDisplay = (weekdays: WeekdayType, hass: HomeAssistant) => {
   function findSequence(list: number[]): number[] {
-    const len: number[] = [];
+    var len: number[] = [];
     for (let i = 0; i < list.length - 1; i++) {
       let j = i + 1;
       while (list[j] - list[j - 1] == 1) j++;
@@ -26,11 +26,11 @@ export const computeDaysDisplay = (weekdays: WeekdayType, hass: HomeAssistant) =
     case EDayType.Weekend:
       return localize('ui.components.date.day_types_long.weekend', getLocale(hass));
     case EDayType.Custom:
-      const list = weekdayToList(weekdays);
-      const seq = findSequence(list);
-      const len = Math.max(...seq);
+      var list = weekdayToList(weekdays);
+      var seq = findSequence(list);
+      var len = Math.max(...seq);
       if (list.length == 6) {
-        const missing = [1, 2, 3, 4, 5, 6, 7].filter(e => !list.includes(e));
+        var missing = [1, 2, 3, 4, 5, 6, 7].filter(e => !list.includes(e));
         return localize(
           'ui.components.date.repeated_days_except',
           getLocale(hass),
@@ -38,9 +38,9 @@ export const computeDaysDisplay = (weekdays: WeekdayType, hass: HomeAssistant) =
           formatWeekday(missing.pop()!, getLocale(hass))
         );
       }
-      const dayNames = list.map(e => formatWeekday(e, getLocale(hass)));
+      var dayNames = list.map(e => formatWeekday(e, getLocale(hass)));
       if (list.length >= 3 && len >= 3) {
-        const start = seq.reduce((obj, e, i) => (e == len ? i : obj), 0);
+        var start = seq.reduce((obj, e, i) => (e == len ? i : obj), 0);
         dayNames.splice(
           start,
           len,
@@ -52,7 +52,7 @@ export const computeDaysDisplay = (weekdays: WeekdayType, hass: HomeAssistant) =
           )
         );
       }
-      const daysString =
+      var daysString =
         dayNames.length > 1
           ? `${dayNames.slice(0, -1).join(', ')} ${hass.localize('ui.common.and')} ${dayNames.pop()}`
           : `${dayNames.pop()}`;
