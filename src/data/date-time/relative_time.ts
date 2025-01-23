fa09e1d9b211be493b48c1c30834ce83d576ecab
scottlavelar <sarahjgeorge97@yahoo.com>
2025-01-23 11:06:46 +0000

    Change build - handling edge cases

--- src/data/date-time/relative_time.ts
+++ src/data/date-time/relative_time.ts
@@ -2,13 +2,13 @@ import { HomeAssistant } from 'custom-card-helpers';
 import { ETimeEvent } from '../../types';
 import { parseRelativeTime, roundTime, stringToTime, timeToString } from './time';
 
-export var relToAbsTime = (timeStr: string, hass: HomeAssistant, options: { stepSize?: number } = {}) => {
-  var res = parseRelativeTime(timeStr);
+export const relToAbsTime = (timeStr: string, hass: HomeAssistant, options: { stepSize?: number } = {}) => {
+  const res = parseRelativeTime(timeStr);
   if (!res) return timeStr;
 
-  var sunEntity = hass.states['sun.sun'];
+  const sunEntity = hass.states['sun.sun'];
 
-  var ts_ref =
+  const ts_ref =
     res.event == 'sunrise'
       ? stringToTime(sunEntity.attributes.next_rising, hass)
       : stringToTime(sunEntity.attributes.next_setting, hass);
@@ -20,25 +20,25 @@ export var relToAbsTime = (timeStr: string, hass: HomeAssistant, options: { step
   return timeToString(ts);
 };
 
-export var absToRelTime = (
+export const absToRelTime = (
   timeStr: string,
   event: ETimeEvent | undefined,
   hass: HomeAssistant,
   options: { stepSize?: number } = {}
 ) => {
-  var res = parseRelativeTime(timeStr);
+  const res = parseRelativeTime(timeStr);
   if (res) timeStr = relToAbsTime(timeStr, hass);
 
-  var ts = stringToTime(timeStr, hass);
+  const ts = stringToTime(timeStr, hass);
 
-  var sunEntity = hass.states['sun.sun'];
+  const sunEntity = hass.states['sun.sun'];
 
-  var ts_sunrise = stringToTime(sunEntity.attributes.next_rising, hass);
-  var ts_sunset = stringToTime(sunEntity.attributes.next_setting, hass);
+  const ts_sunrise = stringToTime(sunEntity.attributes.next_rising, hass);
+  const ts_sunset = stringToTime(sunEntity.attributes.next_setting, hass);
 
   if (!event) event = Math.abs(ts - ts_sunrise) < Math.abs(ts - ts_sunset) ? ETimeEvent.Sunrise : ETimeEvent.Sunset;
 
-  var ts_ref =
+  const ts_ref =
     event == ETimeEvent.Sunrise
       ? stringToTime(sunEntity.attributes.next_rising, hass)
       : stringToTime(sunEntity.attributes.next_setting, hass);
