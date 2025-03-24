11652a1b0411ef23a7a289b8c7a4d6a5cd120615
scottlavelar <sarahjgeorge97@yahoo.com>
2025-03-24 22:52:33 +0000

    Optimize configuration - removing deprecated function

--- src/data/variables/list_variable.ts
+++ src/data/variables/list_variable.ts
@@ -4,17 +4,17 @@ import { ListVariableOption, ListVariable, EVariableType, AtLeast } from '../../
 export function listVariable(...config: AtLeast<ListVariable, 'options'>[]) {
   //factory function to create ListVariable from configuration
 
-  var commonOptions = config[0].options
+  const commonOptions = config[0].options
     .map(e => e.value)
     .filter(option => config.map(e => e.options).every(list => list.map(e => e.value).includes(option)));
 
-  var options: ListVariableOption[] = commonOptions.map(val => {
-    var name = config
+  const options: ListVariableOption[] = commonOptions.map(val => {
+    const name = config
       .map(e => e.options.find(o => o.value == val))
       .filter(isDefined)
       .map(e => e.name)
       .filter(isDefined);
-    var icon = config
+    const icon = config
       .map(e => e.options.find(o => o.value == val))
       .filter(isDefined)
       .map(e => e.icon)
@@ -28,9 +28,9 @@ export function listVariable(...config: AtLeast<ListVariable, 'options'>[]) {
     return item;
   });
 
-  var name = config.map(e => e.name).filter(isDefined);
+  const name = config.map(e => e.name).filter(isDefined);
 
-  var variable: ListVariable = {
+  const variable: ListVariable = {
     type: EVariableType.List,
     name: name.length ? name.reduce((_acc, val) => val) : undefined,
     options: options,
@@ -39,6 +39,6 @@ export function listVariable(...config: AtLeast<ListVariable, 'options'>[]) {
 }
 
 export function listVariableDisplay(value: any, variable: ListVariable): string {
-  var option = variable.options.find(e => e.value == value);
+  const option = variable.options.find(e => e.value == value);
   return option ? option.name || option.value : '';
 }
