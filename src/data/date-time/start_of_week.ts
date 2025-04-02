ab4c3194f4b128300feaf901dfc695e608347b82
scottlavelar <sarahjgeorge97@yahoo.com>
2025-04-02 12:55:30 +0000

    Add styles - updating docs

--- src/data/date-time/start_of_week.ts
+++ src/data/date-time/start_of_week.ts
@@ -1,17 +1,17 @@
 export function startOfWeek(locale) {
-  let parts = locale.match(
+  const parts = locale.match(
     /^([a-z]{2,3})(?:-([a-z]{3})(?=$|-))?(?:-([a-z]{4})(?=$|-))?(?:-([a-z]{2}|\d{3})(?=$|-))?/i
   );
 
-  let language = parts[1];
-  let region = parts[4];
+  const language = parts[1];
+  const region = parts[4];
 
-  let regionSat = 'AEAFBHDJDZEGIQIRJOKWLYOMQASDSY'.match(/../g)!;
-  let regionSun = 'AGARASAUBDBRBSBTBWBZCACNCODMDOETGTGUHKHNIDILINJMJPKEKHKRLAMHMMMOMTMXMZNINPPAPEPHPKPRPTPYSASGSVTHTTTWUMUSVEVIWSYEZAZW'.match(
+  const regionSat = 'AEAFBHDJDZEGIQIRJOKWLYOMQASDSY'.match(/../g)!;
+  const regionSun = 'AGARASAUBDBRBSBTBWBZCACNCODMDOETGTGUHKHNIDILINJMJPKEKHKRLAMHMMMOMTMXMZNINPPAPEPHPKPRPTPYSASGSVTHTTTWUMUSVEVIWSYEZAZW'.match(
     /../g
   )!;
-  let languageSat = ['ar', 'arq', 'arz', 'fa'];
-  let languageSun = 'amasbndzengnguhehiidjajvkmknkolomhmlmrmtmyneomorpapssdsmsnsutatethtnurzhzu'.match(/../g)!;
+  const languageSat = ['ar', 'arq', 'arz', 'fa'];
+  const languageSun = 'amasbndzengnguhehiidjajvkmknkolomhmlmrmtmyneomorpapssdsmsnsutatethtnurzhzu'.match(/../g)!;
 
   if (region) return regionSun.includes(region) ? 'sun' : regionSat.includes(region) ? 'sat' : 'mon';
   else return languageSun.includes(language) ? 'sun' : languageSat.includes(language) ? 'sat' : 'mon';
